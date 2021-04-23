f546ea73da127881627480562ab4f7535db9db58
Dunieski Otano <dunieskior@gmail.com>
2021-04-23 14:26:25 -0400

    Challenge 5 Part 1

---
+++ Challenge5_Part1.py
@@ -0,0 +1,33 @@
+# 1. Bryan -> Give a demo of nested loops (please make it different from the one
+# in class)
+
+
+
+# 2. Shweta -> Give a demo of nested if statements
+
+
+
+
+# 3. Yukta -> Create a function that accepts different types and print the values
+
+
+
+
+
+# 4. Donovan -> List of dictionaries (3) [manually]
+
+
+
+
+# 5. Fatine -> give a demo of nested while loops (3)
+
+
+
+
+# 6. Namratha -> Give a demo of exceptions
+
+
+
+
+
+# 7. Rafil -> in a list from 1 to 20, print all numbers except 9, 13
